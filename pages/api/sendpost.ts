3a516af21625358ffac28bfa93d0ee1941e6779d
Bartosz Czaplicki <h3ronnn@gmail.com>
2022-11-03 19:24:53 +0100

    Delete unused api routes

--- pages/api/sendpost.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import conn from "../../lib/db";
-
-const sendPost = async (req: NextApiRequest, res: NextApiResponse) => {};
-
-export default sendPost;
